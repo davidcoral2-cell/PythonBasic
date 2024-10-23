1a3dcd21ca90e025ed72d6c302cd3e10c5e42841
“davidcoral2-cell” <gtbyy2@gmail.com>
2024-10-23 18:48:41 +0200

    ex 14

--- Exercici14.py
+++ Exercici14.py
@@ -137,14 +137,14 @@ def grandetres():
     h = int(input("Dime otro numero "))
     j = int(input("Dime un tercero "))
 
-    if g > h and g > j:
-        print("{} es mayor que {} y {} ".format(g, h, j))
-    elif h > g and h > j:
-        print("{} es mayor que {} y {} ".format(h, g, j))
-    elif j > g and j > h:
-        print("{} es mayor que {} y {} ".format(j, g, h))
-    else:
-        print("Los tres numeros son iguales.")
+    if g == h and h == j:
+        print("Los tres números son iguales.")
+    elif g >= h and g >= j: 
+        print("{} es mayor que {} y {}.".format(g, h, j))
+    elif h >= g and h >= j:
+        print("{} es mayor que {} y {}.".format(h, g, j))
+    elif j >= g and j >= h:
+        print("{} es mayor que {} y {}.".format(j, g, h))
         
 
 
